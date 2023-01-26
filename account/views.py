0a0e2c4b98c44b8dd9792dce5126fb65c56f2d03
faridhaavasi <faridhavasi777@gmail.com>
2023-01-27 01:19:59 +0330

    test login view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import View
 # Create your views here.
+
+class user_login(View):
+    def get(self, request):
+        pass
+
+
+def login(request):
+    return render(request, 'account/login.html', {})
