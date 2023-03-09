1ccd62dc1ca09d7aeedec24c4bebbbcf9763483c
faridhaavasi <faridhavasi777@gmail.com>
2023-03-09 15:47:50 +0330

    reset project

--- account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-app_name='account'
-urlpatterns=[
-    path('login', views.user_login.as_view(),name='login'),
-    path('register',views.User_register.as_view(),name='register'),
-
-
-]
--- web/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'web'
-urlpatterns = [
-    path('', views.home,  name='home'),
-    path('edit/<int:pk>', views.Update_info.as_view(), name='edit'),
-    path('index', views.index.as_view(), name='index'),
-]
